f9e269cc2cc717045011dd50f95a2aee797b524b
SRIOUSS <changhui5796@gmail.com>
2026-07-10 09:50:37 +0900

    Initial commit

---
+++ src/main/java/com/nhnacademy/insightonauth/InsightonAuthApplication.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.insightonauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsightonAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InsightonAuthApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nhnacademy/insightonauth/InsightonAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.insightonauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InsightonAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
