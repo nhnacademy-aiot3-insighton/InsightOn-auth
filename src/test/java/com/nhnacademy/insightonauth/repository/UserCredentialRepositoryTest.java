a587e04c58b3821d80fab5b0087bc6f362d47aca
Jungeunsun565 <eunsun565@gmail.com>
2026-08-09 20:16:03 +0900

    test: Repository 테스트에 다른 사용자 데이터 검증 추가

--- src/test/java/com/nhnacademy/insightonauth/repository/UserCredentialRepositoryTest.java
+++ src/test/java/com/nhnacademy/insightonauth/repository/UserCredentialRepositoryTest.java
@@ -20,19 +20,24 @@ class UserCredentialRepositoryTest {
     @Autowired
     private UserRepository userRepository;
 
-    private User user;
+    private User user1;
+    private User user2;
 
     @BeforeEach
     void setUp() {
-        user = new User("test@test.com", "test", "01012345678");
-        userRepository.save(user);
-        userCredentialRepository.save(new UserCredential(user, "hashed-password"));
+        user1 = new User("test@test.com", "test", "01012345678");
+        userRepository.save(user1);
+        userCredentialRepository.save(new UserCredential(user1, "hashed-password"));
+
+        user2 = new User("other@test.com", "other", "01099998888");
+        userRepository.save(user2);
+        userCredentialRepository.save(new UserCredential(user2, "other-hashed-password"));
     }
 
     @Test
     @DisplayName("user로 존재 여부 확인 - true")
     void existsByUser_returnsTrueWhenExists() {
-        boolean exists = userCredentialRepository.existsByUser(user);
+        boolean exists = userCredentialRepository.existsByUser(user1);
 
         assertThat(exists).isTrue();
     }
@@ -40,18 +45,18 @@ class UserCredentialRepositoryTest {
     @Test
     @DisplayName("user로 존재 여부 확인 - false")
     void existsByUser_returnsFalseWhenNotExists() {
-        User otherUser = new User("other@test.com", "other", "01099998888");
-        userRepository.save(otherUser);
+        User noCredentialUser = new User("nocred@test.com", "nocred", "01077776666");
+        userRepository.save(noCredentialUser);
 
-        boolean exists = userCredentialRepository.existsByUser(otherUser);
+        boolean exists = userCredentialRepository.existsByUser(noCredentialUser);
 
         assertThat(exists).isFalse();
     }
 
     @Test
     @DisplayName("user로 인증 정보 조회")
-    void findByUser_returnsUserCredential() {
-        Optional<UserCredential> found = userCredentialRepository.findByUser(user);
+    void findByUser_returnsOwnUserCredential() {
+        Optional<UserCredential> found = userCredentialRepository.findByUser(user1);
 
         assertThat(found)
                 .isPresent()
@@ -61,12 +66,24 @@ class UserCredentialRepositoryTest {
     }
 
     @Test
-    @DisplayName("인증 정보 없는 user 조회 시 빈 값 반환")
-    void findByUser_whenNotExists_returnsEmpty() {
-        User otherUser = new User("other@test.com", "other", "01099998888");
-        userRepository.save(otherUser);
+    @DisplayName("다른 사용자 인증 정보 조회")
+    void findByUser_returnsOtherUsersOwnCredential() {
+        Optional<UserCredential> found = userCredentialRepository.findByUser(user2);
 
-        Optional<UserCredential> found = userCredentialRepository.findByUser(otherUser);
+        assertThat(found)
+                .isPresent()
+                .get()
+                .extracting(UserCredential::getPasswordHash)
+                .isEqualTo("other-hashed-password");
+    }
+
+    @Test
+    @DisplayName("인증 정보 없는 user 조회")
+    void findByUser_whenNotExists_returnsEmpty() {
+        User noCredentialUser = new User("nocred@test.com", "nocred", "01077776666");
+        userRepository.save(noCredentialUser);
+
+        Optional<UserCredential> found = userCredentialRepository.findByUser(noCredentialUser);
 
         assertThat(found).isEmpty();
     }
