0d3dec9d3952aa998368f5ae032c98e90a146b5d
Jungeunsun565 <eunsun565@gmail.com>
2026-08-20 10:33:50 +0900

    test: UserCredentialServiceImpl 테스트 작성

--- src/main/java/com/nhnacademy/insightonauth/service/impl/UserCredentialServiceImpl.java
+++ src/main/java/com/nhnacademy/insightonauth/service/impl/UserCredentialServiceImpl.java
@@ -11,7 +11,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.OffsetDateTime;
-import java.time.ZoneOffset;
 
 @Service
 @Transactional
